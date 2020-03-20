1019fb59ecbf90e0f15c5719c19c0955c27422ff
Mikey Robinson <Mlrobinson1993@gmail.com>
2020-03-20 13:21:17 +0000

    styled components removed

--- src/components/HeroBlock.js
+++ src/components/HeroBlock.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import _ from 'lodash';
-import styled from 'styled-components';
 
 import { markdownify } from '../utils';
 
@@ -11,14 +10,15 @@ export default class HeroBlock extends React.Component {
 				id={_.get(this.props, 'section.section_id')}
 				className='block hero-block outer'
 			>
-				<Container>
-					<ImageContainer>
-						<Img
+				<div className='hero-container'>
+					<div className='hero_image-container'>
+						<img
+							className='hero_image'
 							loading='lazy'
 							alt='photograph of Mikey standing in front of a colourful wall'
 							src={_.get(this.props, 'section.image')}
 						/>
-					</ImageContainer>
+					</div>
 					<div className='inner-small'>
 						<div className='block-header'>
 							<h2>{_.get(this.props, 'section.title')}</h2>
@@ -27,7 +27,7 @@ export default class HeroBlock extends React.Component {
 							<div className='block-copy'>
 								{markdownify(_.get(this.props, 'section.content'))}
 							</div>
-							<BtnContainer>
+							<div className='hero_button-container'>
 								<a
 									class='button'
 									href={_.get(this.props, 'section.resumeURL')}
@@ -42,55 +42,11 @@ export default class HeroBlock extends React.Component {
 								>
 									Check out my Github
 								</a>
-							</BtnContainer>
+							</d>
 						</div>
 					</div>
-				</Container>
+				</div>
 			</section>
 		);
 	}
 }
-
-const Container = styled.div`
-	margin: 0 auto;
-	width: 90vw;
-	display: flex;
-	align-items: center;
-
-	@media (max-width: 1000px) {
-		flex-direction: column;
-	}
-`;
-const ImageContainer = styled.div`
-	width: 25vw;
-	height: 25vw;
-	margin: 0px auto;
-	overflow: hidden;
-	border-radius: 50%;
-	border: 4px solid white;
-	box-shadow: rgba(0, 0, 0, 0.2) 0px 6px 12px;
-
-	@media (max-width: 1000px) {
-		width: 35vw;
-		height: 35vw;
-		margin-bottom: 2rem;
-	}
-`;
-
-const Img = styled.img`
-	width: 100%;
-	max-width: auto;
-	margin: 0 auto;
-	height: auto;
-`;
-
-const BtnContainer = styled.div`
-	a {
-		margin: 1rem 1rem 1rem 0;
-	}
-
-	@media (max-width: 580px) {
-		a {
-			margin: 1rem 0;
-	}
-`;
